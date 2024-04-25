573c42cecbb2bea152a8af40d604a520d10183d4
Patrick Lauwers <patrick@belgaco.be>
2024-04-25 03:09:41 +0200

    flash message flex and hero blade fex

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,5 +1,5 @@
 <section
-class="relative h-72 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4"
+class="relative h-72 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4" style="min-height: 50vh;
 >
 <div
     class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
@@ -31,3 +31,5 @@ class="relative h-72 bg-laravel flex flex-col justify-center align-center text-c
     
 </div>
 </section>
+
+
