b16f4868e89e677a348b0546beb63266c0bb0cec
Patrick Lauwers <patrick@belgaco.be>
2024-04-25 02:54:16 +0200

    manage or sign up links

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -8,17 +8,26 @@ class="relative h-72 bg-laravel flex flex-col justify-center align-center text-c
 
 <div class="z-10">
     <h1 class="text-6xl font-bold uppercase text-white">
-        Hollandico<span class="text-black">Luxury cars</span>
+        Hollandico <span class="text-black">Luxury cars</span>
     </h1>
     <p class="text-2xl text-gray-200 font-bold my-4">
         Find your Luxury car
     </p>
     <div>
-        <a
-            href="register.html"
-            class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
-            >Sign Up to List a luxury car</a
-        >
+        @if(auth()->check())
+            <!-- User is signed in -->
+            <a href="/listings/manage"
+               class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">
+                Manage Your Listings
+            </a>
+        @else
+            <!-- User is not signed in -->
+            <a href="/register"
+               class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">
+                Sign Up to List a Luxury Car
+            </a>
+        @endif
     </div>
+    
 </div>
 </section>
